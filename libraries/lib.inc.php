f8f4a08bbaa1e87e1dfefeaf0e7254290fc09ea3
ReimuHakurei <reimu@reimuhakurei.net>
2022-06-01 22:19:25 +0000

    Bump version.

--- libraries/lib.inc.php
+++ libraries/lib.inc.php
@@ -16,7 +16,7 @@
 	$appName = 'phpPgAdmin';
 
 	// Application version
-	$appVersion = '7.14.1-mod';
+	$appVersion = '7.14.2-mod';
 
 	// PostgreSQL and PHP minimum version
 	global $postgresqlMinVer;

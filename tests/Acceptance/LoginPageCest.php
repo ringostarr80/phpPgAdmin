b5e3464a5a11f95064c0ad8b7a71fcb4e057404f
ringostarr80 <ringo19@gmx.de>
2025-03-17 19:51:53 +0100

    renamed test-directory and added test

---
+++ tests/Acceptance/LoginPageCest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+final class LoginPageCest
+{
+    // @phpcs:disable
+    public function _before(AcceptanceTester $i): void
+    {
+        // Code here will be executed before each test.
+    }
+
+    public function tryToTestLoginFailed(AcceptanceTester $i): void
+    {
+        $i->amOnPage('/');
+
+        $i->switchToIframe('browser');
+        $i->waitForText('PostgreSQL Test');
+        $i->click('PostgreSQL Test');
+
+        $i->switchToIframe();
+        $i->switchToIframe('detail');
+
+        $i->seeInFormFields(
+            'form[name="login_form"]',
+            [
+                'loginUsername' => '',
+                'loginPassword_' . hash('sha256', 'PostgreSQL Test') => '',
+            ]
+        );
+
+        $i->submitForm('form[name="login_form"]', [
+            'loginUsername' => 'admin',
+            'loginPassword_' . hash('sha256', 'PostgreSQL Test') => 'wrongpassword',
+        ]);
+
+        $i->makeScreenshot('login_failed');
+
+        $i->waitForText('Login failed');
+    }
+}

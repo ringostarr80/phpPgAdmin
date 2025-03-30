1e642ac764053c6abcd01f4718c04489d0a78bc3
ringostarr80 <ringo19@gmx.de>
2025-03-30 18:58:01 +0200

    test

--- src/Website/Login.php
+++ src/Website/Login.php
@@ -69,6 +69,7 @@ class Login extends Website
                 //'platform' => 'PostgreSQL ' . $server->Version->Value,
                 //'pgVersion' => $server->Version->Value
             ];
+            error_log('webdbLogin: ' . print_r($_SESSION['webdbLogin'], true));
 
             $redirectLocationUrlParams = [
                 'server' => $loginServer

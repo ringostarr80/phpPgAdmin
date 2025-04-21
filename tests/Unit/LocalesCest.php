ed2b049ddc0ecd850f994eef9b91b021a20e34ed
ringostarr80 <ringo19@gmx.de>
2025-04-21 13:15:49 +0200

    Added Unit tests for locales.

---
+++ tests/Unit/LocalesCest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PhpPgAdmin\Config;
+use Tests\Support\UnitTester;
+
+final class LocalesCest
+{
+    // @phpcs:disable
+    public function _before(UnitTester $I): void
+    {
+        // Code here will be executed before each test.
+    }
+
+    public function tryToTestLocales(UnitTester $i): void
+    {
+        $locales = Config::getAvailableLocales();
+        $i->assertNotEmpty($locales, 'No locales found');
+        $i->assertEquals(28, count($locales), 'Expected 28 locales');
+    }
+}

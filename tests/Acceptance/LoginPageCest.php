980726832bff18eff9de273972b695db60c1c7ef
ringostarr80 <ringo19@gmx.de>
2025-05-10 09:22:18 +0200

    Test for CI.

--- tests/Acceptance/LoginPageCest.php
+++ tests/Acceptance/LoginPageCest.php
@@ -12,7 +12,7 @@ final class LoginPageCest
 {
     public const LOGIN_FORM_SELECTOR = 'form[name="login_form"]';
 
-    /*
+    //*
     public function tryToTestLoginFailed(AcceptanceTester $i): void
     {
         $i->amOnPage('/');
@@ -41,7 +41,7 @@ final class LoginPageCest
     }
     //*/
 
-    /*
+    //*
     public function tryToTestLoginSuccessful(AcceptanceTester $i): void
     {
         $loginUsername = MyConfigExtension::getEnvVar('PHPPGADMIN_TEST_SERVER_USERNAME') ?? 'postgres';
